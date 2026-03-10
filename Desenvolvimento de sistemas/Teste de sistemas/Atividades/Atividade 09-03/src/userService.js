4c283c94103cf5e79cce9a3baf9c768040f71344
WenzelJoao <jvitorwenzel@gmail.com>
2026-03-09 21:50:27 -0300

    a

---
+++ Desenvolvimento de sistemas/Teste de sistemas/Atividades/Atividade 09-03/src/userService.js
@@ -0,0 +1,19 @@
+export function createUser(userData) {
+    if(!userData.name) {
+        throw new Error("O nome do usuário é obrigatório.")
+    }
+
+    if(userData.age < 18) {
+        throw new Error("O usuário deve ser maior de idade.")
+    }
+
+    const newUser = {
+        id: Math.floor(Math.random() * 1000),
+        name: userData.name,
+        age: userData.age,
+        isActive: true,
+        roles: ["user"],
+    }
+
+    return newUser
+}
