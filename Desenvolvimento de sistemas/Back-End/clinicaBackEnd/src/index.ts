c64c2d5119cf1b8461fe7e196108b45f7baeb467
WenzelJoao <jvitorwenzel@gmail.com>
2026-04-09 21:55:04 -0300

    att divisão de rotas

--- Desenvolvimento de sistemas/Back-End/clinicaBackEnd/src/index.ts
+++ Desenvolvimento de sistemas/Back-End/clinicaBackEnd/src/index.ts
@@ -1,11 +1,9 @@
 import express from 'express';
-import { prisma } from './prisma/prisma';
-import type { Exame, Usuario, Tipo_token } from './prisma/generated/prisma/client';
-import { createHash } from './utils/createHash';
-import bcrypt from "bcrypt";
-import { signTokenAcesso, signTokenRefresh } from './utils/jwt';
 import { auth } from './middleware/auth';
 import cors from "cors"
+import { authRouter } from './routes/auth';
+import { userRouter } from './routes/user';
+import { examRouter } from './routes/exam';
 
 const app = express();
 app.use(express.json())
@@ -17,204 +15,13 @@ app.get('/', (req, res) => {
   res.send("Hello world")
 })
 
-//autenticacao
-app.post("/cadastro", async (req, res) => {
-  const dadosUsuario = req.body as Usuario
-  const hash = await createHash(dadosUsuario.senha);
-  const usuarioCriado = await prisma.usuario.create({
-    data: {
-      email: dadosUsuario.email,
-      nome: dadosUsuario.nome || null,
-      senha: hash
-    }
-  })
-  return res.status(201).json({
-    message: "Usuário criado com sucesso!",
-    data: usuarioCriado
-  })
-})
-
-app.post("/login", async (req, res) => {
-  const dadosUsuario = req.body as Partial<Usuario>
-  const existeUsuario = await prisma.usuario.findUnique({
-    where: {
-      email: dadosUsuario.email || '',
-    }
-  })
-  const credenciaisValidas = await bcrypt.compare(dadosUsuario.senha || "", existeUsuario?.senha || "")
-
-  if (existeUsuario && credenciaisValidas) {
-    const tokenAcesso = signTokenAcesso({
-      email: existeUsuario.email,
-      nome: existeUsuario.nome
-    })
-    const tokenRefresh = signTokenRefresh({
-      email: existeUsuario.email,
-      nome: existeUsuario.nome
-    })
-
-    const accessExpires = new Date()
-    const accessExpiresUpdate = accessExpires.setHours(accessExpires.getHours() + 1)
-    // acesso create
-    await prisma.token.create({
-      data: {
-        token: tokenAcesso,
-        expira_em: new Date(accessExpiresUpdate),
-        tipo: 'ACESSO',
-        usuarioId: existeUsuario.id
-      }
-    })
-
-    //refresh create
-    const refreshExpires = new Date()
-    const refreshExpiresUpdated = refreshExpires.setMonth(refreshExpires.getMonth() + 1)
-
-    await prisma.token.create({
-      data: {
-        token: tokenRefresh,
-        expira_em: new Date(refreshExpiresUpdated),
-        tipo: 'REFRESH',
-        usuarioId: existeUsuario.id
-      }
-    })
-
-    return res.status(200).json({
-      message: "Usuário autenticado com sucesso!",
-      accessToken: tokenAcesso,
-      refreshToken: tokenRefresh
-    })
-  }
-
-  return res.status(401).json({
-    message: "Credenciais inválidas"
-  })
-
-})
+app.use(authRouter)
+app.use(userRouter)
+app.use(examRouter)
 
 app.use(auth)
 
-// Endpoints usuario
-app.get('/usuarios', async (_, res) => {
-  const usuarios = await prisma.usuario.findMany();
-  return res.json(usuarios);
-})
 
-app.get('/usuarios/:id', async (req, res) => {
-  const idUsuario = Number(req.params.id)
-  const usuario = await prisma.usuario.findUnique({
-    where: {
-      id: idUsuario
-    }
-  })
-
-  return res.status(200).json(usuario);
-})
-
-app.post("/usuarios", async (req, res) => {
-  const dadosUsuario = req.body as Usuario
-  const hash = await createHash(dadosUsuario.senha);
-  const usuarioCriado = await prisma.usuario.create({
-    data: {
-      email: dadosUsuario.email,
-      nome: dadosUsuario.nome || null,
-      senha: hash
-    }
-  })
-  return res.status(201).json(usuarioCriado)
-})
-
-
-app.put("/usuarios/:id", async (req, res) => {
-  const idUsuario = Number(req.params.id)
-  const dadosParaAtualizar = req.body as Omit<Usuario, 'id'>
-
-  const usuarioAtualizado = await prisma.usuario.update({
-    data: {
-      ...dadosParaAtualizar
-    },
-    where: {
-      id: idUsuario
-    }
-  })
-
-  return res.status(200).json(usuarioAtualizado);
-})
-
-app.delete('/usuarios/:id', async (req, res) => {
-  const idUsuario = Number(req.params.id)
-  const usuarioDeletado = await prisma.usuario.delete({
-    where: {
-      id: idUsuario
-    }
-  })
-
-  return res.status(200).json({
-    mensagem: "Usuário deletado com sucesso!",
-    data: usuarioDeletado
-  });
-})
-
-//Exames
-app.get('/exames', async (_, res) => {
-  const exames = await prisma.exame.findMany();
-  return res.json(exames);
-})
-
-app.get('/exames/:id', async (req, res) => {
-  const idExame = Number(req.params.id)
-  const exame = await prisma.exame.findUnique({
-    where: {
-      id: idExame
-    }
-  })
-
-  return res.status(200).json(exame);
-})
-
-app.post("/exames", async (req, res) => {
-  const dadosExame = req.body as Exame
-  const exameCriado = await prisma.exame.create({
-    data: {
-      tipo_exame: dadosExame.tipo_exame,
-      valor: dadosExame.valor,
-      descricao: dadosExame.descricao,
-      data_exame: new Date(dadosExame.data_exame),
-      resultado: dadosExame.resultado
-    }
-  })
-  return res.status(201).json(exameCriado)
-})
-
-app.put("/exames/:id", async (req, res) => {
-  const idExame = Number(req.params.id)
-  const dadosParaAtualizar = req.body as Omit<Exame, 'id'>
-
-  const exameAtualizado = await prisma.exame.update({
-    data: {
-      ...dadosParaAtualizar,
-      data_exame: new Date(dadosParaAtualizar.data_exame)
-    },
-    where: {
-      id: idExame
-    }
-  })
-
-  return res.status(200).json(exameAtualizado);
-})
-
-app.delete('/exames/:id', async (req, res) => {
-  const idExame = Number(req.params.id)
-  const exameDeletado = await prisma.exame.delete({
-    where: {
-      id: idExame
-    }
-  })
-
-  return res.status(200).json({
-    mensagem: "Exame deletado com sucesso!",
-    data: exameDeletado
-  });
-})
 
 app.listen(port, () => {
   console.log("Servidor ta de pé :p")
--- Desenvolvimento de sistemas/Back-End/clinicaFrontEnd/src/App.tsx
+++ Desenvolvimento de sistemas/Back-End/clinicaFrontEnd/src/App.tsx
@@ -1,99 +1,34 @@
-import axios from 'axios'
-import { use, useState } from 'react'
+import { useEffect, useState } from 'react'
+import { Login } from './Login'
+import { Registrar } from './Registrar';
+import { Home } from './Home';
 
 function App() {
-  const [email, setEmail] = useState("")
-  const [senha, setSenha] = useState("")
-  const [dataLogin, setDataLogin] = useState(null)
-  const [dataCadastro, setDataCadastro] = useState(false)
-  const [nome, setNome] = useState("")
+  const [dataLogin, setDataLogin] = useState<any>(null);
+  const [registrar, setRegistrar] = useState(false);
 
-
-
-
-  const logar = async () => {
-    try {
-      const response = await axios.post("http://localhost:3000/login", {
-        email,
-        senha
+  useEffect(() => {
+    const accessToken = localStorage.getItem("tokenAcesso")
+    const refreshToken = localStorage.getItem("tokenRefresh")
+    const hasTokens = accessToken && refreshToken
+    if (hasTokens) {
+      setDataLogin({
+        accessToken,
+        refreshToken
       })
-      if (response?.data) {
-        setDataLogin(response.data)
-      }
-    } catch (error) {
-      console.log(error)
-      alert("Erro ao fazer o login, verifique suas credenciais!")
     }
-
-  }
-
-  const cadastrar = async (e: any) => {
-    e.preventDefault()
-    try {
-      const response = await axios.post("http://localhost:3000/cadastro", {
-        nome,
-        email,
-        senha
-      })
-      if (response?.data) {
-        setDataCadastro(response.data)
-        setNome("")
-        setEmail("")
-        setSenha("")
-
-      }
-    } catch (error) {
-      console.log(error);
-      alert("Erro ao fazer o cadastro, verifique suas credenciais!")
-
-    }
-  }
-
+  }, [])
 
   return (
     <>
-      {dataLogin !== null ? (
-        <div className='flex flex-col bg-gray-400 w-full pt-32 items-center h-screen'>
-          <h1 className='text-2xl font-semibold text-white'>Seja bem-vindo fulaninho</h1>
-          <button className='bg-red-800 px-5 py-2 text-white rounded-2xl' onClick={() => setDataLogin(null)}>Sair</button>
-        </div>
-      ) : dataCadastro ? (
-        < div className='flex flex-col bg-gray-400 w-full justify-center items-center h-screen'>
-          <form className='flex  gap-2 w-full flex-col items-center justify-center'>
-            <label htmlFor="email">Email</label>
+      {dataLogin !== null ? <Home setDataLogin={setDataLogin} /> : registrar ?
+        <Registrar setRegistrar={setRegistrar} />
+        :
+        <Login
+          setDataLogin={setDataLogin}
+          setRegistrar={setRegistrar}
+        />
 
-            <input className='cadastro_login' type="email" onChange={(e) => setEmail(e.target.value)} name="email" id="" />
-
-            <label htmlFor="password">Senha</label>
-
-            <input className='cadastro_login' type="password" onChange={(e) => setSenha(e.target.value)} name="password" id="" />
-          </form>
-
-            <a href="#" onClick={()=> setDataCadastro(!dataCadastro)}>Possui cadastro?</a>
-
-          <button onClick={logar} className='bg-gray-800 px-5 py-2 text-white rounded-2xl'>Login</button>
-
-        </div >
-      ) : (
-        <div className='flex flex-col bg-gray-400 w-full justify-center items-center h-screen'>
-          <h1 className='text-4xl font-bold text-gray-800 p-5'>Cadastro</h1>
-          <form className='flex  gap-2 w-full flex-col items-center justify-center'>
-            <label htmlFor="name">Nome</label>
-            <input className='cadastro_login' type="text" onChange={(e) => setNome(e.target.value)} name='name' id='' />
-
-            <label htmlFor="email">Email</label>
-            <input className='cadastro_login' type="email" onChange={(e) => setEmail(e.target.value)} name='email' id='' />
-
-            <label htmlFor="password">Senha</label>
-            <input className='cadastro_login' type="password" onChange={(e) => setSenha(e.target.value)} name='password' id='' />
-
-            <a href="#" onClick={()=> setDataCadastro(!dataCadastro)}>Possui login?</a>
-
-            <button type="submit" onClick={cadastrar} className='bg-gray-800 px-5 py-2 text-white rounded-2xl'>Cadastre-se</button>
-
-          </form>
-        </div>
-      )
       }
 
     </>
